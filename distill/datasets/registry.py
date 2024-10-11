91740b4b2f3297ab876749bfeae3d3adc0b69bf9
yds <2338853081@qq.com>
2024-10-11 14:41:52 +0800

    支持torch数据集

--- distill/datasets/registry.py
+++ distill/datasets/registry.py
@@ -15,16 +15,29 @@ DATASET_WRAPPER_DICT = dict()
 
 # 动态导入 gammagl.datasets 包
 gammagl_datasets_module = importlib.import_module('gammagl.datasets')
+pyg_datasets_module = importlib.import_module('torch_geometric.datasets')
 
 # 确保 gammagl.datasets 模块现在在 sys.modules 中
 assert 'gammagl.datasets' in sys.modules
+DATASET_DICT.update(misc_util.get_classes_as_dict('torch_geometric.datasets'))
 DATASET_DICT.update(misc_util.get_classes_as_dict('gammagl.datasets'))
 
 
 def get_dataset(key, *args, **kwargs):
     if key in DATASET_DICT:
-        return DATASET_DICT[key](*args, **kwargs)
-    raise ValueError('dataset_name `{}` is not expected'.format(key))
+        dataset_class = DATASET_DICT[key]
+        # 检查该数据集类是否需要 'name' 参数，若不需要则删除
+        if 'name' in kwargs:
+            try:
+                # 检测是否支持 'name' 参数
+                return dataset_class(*args, **kwargs)
+            except TypeError:
+                # 如果抛出 TypeError，说明不需要 'name' 参数，因此删除它
+                kwargs.pop('name')
+
+        return dataset_class(*args, **kwargs)
+    else:
+        raise ValueError(f"dataset_name '{key}' is not expected")
 
 
 def register_dataset(arg=None, **kwargs):
